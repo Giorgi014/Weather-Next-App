870f52846af4e9120f94094047f72d0ebf07d2ae
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-11-02 02:42:28 +0400

    first commit

---
+++ src/app/main/page.tsx
@@ -0,0 +1,11 @@
+import { Search } from "../search/page"
+
+export const Main = () => {
+    return (
+        <main>
+            <h1>Weather Now</h1>
+            <p>Your beautiful weather companion</p>
+            <Search/>
+        </main>
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Header } from "./header/page";
+import { Main } from "./main/page";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Main />
+    </div>
+  );
+}
