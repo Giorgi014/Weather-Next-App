a1dcdcd62a8913adf7f503cdc700819ab2e618d8
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-11-02 23:15:28 +0400

    feat: Start header component

--- src/app/header/page.tsx
+++ src/app/header/page.tsx
@@ -1,7 +1,11 @@
+import { Mode } from "./mode/page";
+
 export const Header = () => {
-    return (
-        <header>
-            header
-        </header>
-    )
-}
+  return (
+    <header className="max_width flex justify-center items-center flex-col">
+      <Mode />
+      <h1>Weather Now</h1>
+      <p>Your beautiful weather companion</p>
+    </header>
+  );
+};
