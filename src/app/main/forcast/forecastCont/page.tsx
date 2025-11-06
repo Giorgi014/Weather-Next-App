ffebea303236033c01cbb9d1f02ec16b4c53b6bc
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-11-07 01:37:40 +0400

    style: Continue refining UI design for 5-day forecast section

--- src/app/main/forcast/forecastCont/page.tsx
+++ src/app/main/forcast/forecastCont/page.tsx
@@ -2,18 +2,18 @@ import { forecastDays } from "./forecast";
 
 const ForecastContainer = () => {
   return (
-    <div className="flex justify-between items-center w-full">
+    <div className="flex justify-around items-center margin_min">
       {forecastDays.map(({ id, day, img, weather, dayTemp, nightTemp }) => (
         <div
           key={id}
-          className="flex justify-between items-center flex-col gap-2.5 w-[clamp(120px, 15vw, 200px)] p-5 blur_bg rounded-[20px] margin_min"
+          className="flex justify-between items-center flex-col gap-2.5 cont_size p-5 blur_bg rounded-[20px] border-amber-50 border-[1px]"
         >
-          <p>{day}</p>
-          <img src={img} alt={img} />
-          <p>{weather}</p>
-          <div className="flex justify-between items-center">
-            <p>{dayTemp}</p>
-            <p>{nightTemp}</p>
+          <p className="day_text_size inter_bold">{day}</p>
+          <img src={img} alt={img} className="img_size" />
+          <p className="weather_text_size inter_medium">{weather}</p>
+          <div className="flex justify-between items-center w-full">
+            <p className="text-[18px] inter_medium">{`${dayTemp}°C`}</p>
+            <p className="text-[18px] inter_medium">{`${nightTemp}°C`}</p>
           </div>
         </div>
       ))}
