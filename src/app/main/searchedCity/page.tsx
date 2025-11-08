955aaab9926ee378b433ac53ca40eb0084ebcb2c
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-11-08 21:19:31 +0400

    feat: Enable C/F unit switching in current weather component

--- src/app/main/searchedCity/page.tsx
+++ src/app/main/searchedCity/page.tsx
@@ -1,6 +1,19 @@
+import { useEffect, useState } from "react";
+import { UseTemp } from "../../hooks/temperatureProvider";
 import InfoContainer from "./infoContainer/page";
 
 const SearchedCity = () => {
+  const [temp, setTemp] = useState<string>("");
+  const { farenheit } = UseTemp();
+
+  useEffect(() => {
+    if (farenheit) {
+      setTemp("F");
+    } else {
+      setTemp("C");
+    }
+  }, [farenheit]);
+
   return (
     <div className="flex justify-center items-center flex-col w-[90%] max-w-[1920px] p-5 rounded-[20px] main blur_bg box_shadow">
       <div className="w-full flex justify-center items-center flex-col">
@@ -16,7 +29,7 @@ const SearchedCity = () => {
               className="w-[clamp(40px,6.6vw,70px)] h-[clamp(40px,6.6vw,70px)]"
             />
             <div className="flex justify-center items-center flex-col text-center">
-              <p className="inter_medium text-[clamp(40px,6.6vw,60px)]">20°C</p>
+              <p className="inter_medium text-[clamp(40px,6.6vw,60px)]">{`20°${temp}`}</p>
               <p className="inter_thin text-[clamp(16px, 2.5vw, 20px)]">
                 CLEAR
               </p>
