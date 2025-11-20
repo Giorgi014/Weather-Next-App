b6abbb201e84eb10498b7686bd1977c4cf6fe245
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-11-21 02:07:33 +0400

    feat: Implement dynamic icons and C/F conversion for 5-day forecast

--- src/app/main/forcast/forecastCont/page.tsx
+++ src/app/main/forcast/forecastCont/page.tsx
@@ -2,6 +2,8 @@
 
 import { useEffect, useState } from "react";
 import ForecastWeather, { ForecastItem } from "./forecastWeather";
+import ForecastWeatherIcon from "./icons";
+import { UseTemp } from "@/src/app/hooks/temperatureProvider";
 
 type WeatherDataProps = {
   weatherData: {
@@ -12,8 +14,19 @@ type WeatherDataProps = {
   };
 };
 
+const kelvinToC = (k: number) => k - 273.15;
+const kelvinToF = (k: number) => ((k - 273.15) * 9) / 5 + 32;
+
+const formatTemp = (k: number, farenheit: boolean) => {
+  if (farenheit) {
+    return `${Math.round(kelvinToF(k))}°F`;
+  }
+  return `${Math.round(kelvinToC(k))}°C`;
+};
+
 const ForecastContainer = ({ weatherData }: WeatherDataProps) => {
   const [forecastDay, setForecastDay] = useState<ForecastItem[]>([]);
+  const { farenheit } = UseTemp();
 
   useEffect(() => {
     if (weatherData) {
@@ -30,11 +43,15 @@ const ForecastContainer = ({ weatherData }: WeatherDataProps) => {
           className="flex justify-between items-center flex-col gap-2.5 cont_size max-md:!w-full p-5 blur_bg rounded-[20px] border-amber-50 border-[1px]"
         >
           <p className="day_text_size inter_bold">{item.dayName}</p>
-          {/* <img src={img} alt={img} className="img_size" /> */}
+          <ForecastWeatherIcon weather={item.weather} className="img_size" />
           <p className="weather_text_size inter_medium">{item.weather}</p>
           <div className="flex justify-between items-center w-full">
-            <p className="text-[18px] inter_medium">{`${Math.round(item.maxTemp)}°C`}</p>
-            <p className="text-[18px] inter_medium">{`${Math.round(item.minTemp)}°C`}</p>
+            <p className="text-[18px] inter_medium">
+              {formatTemp(item.maxTemp, farenheit)}
+            </p>
+            <p className="text-[18px] inter_medium">
+              {formatTemp(item.minTemp, farenheit)}
+            </p>
           </div>
         </div>
       ))}
